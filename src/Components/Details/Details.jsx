7e9506aaa380c965b2d888cd8ae525b3ce8bcfb5
HjRimon <97096525+HjRimon@users.noreply.github.com>
2023-11-12 12:03:35 +0600

    details card update

--- src/Components/Details/Details.jsx
+++ src/Components/Details/Details.jsx
@@ -1,28 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useLoaderData, useParams } from "react-router-dom";
-// import DetailsCard from "./DetailsCard";
-
-// const Details = () => {
-//   const [detail, setDetail] = useState({});
-
-//   const { id } = useParams();
-
-//   const details = useLoaderData();
-
-//   useEffect(() => {
-//     const FindDetails = details?.find((detail) => detail._id === id);
-//     setDetail(FindDetails);
-//   }, [id, details]);
-
-//   return (
-//     <div className="h-[80vh]">
-//       <DetailsCard detail={detail}></DetailsCard>
-//     </div>
-//   );
-// };
-
-// export default Details;
-
 const Details = () => {
   return <div></div>;
 };
